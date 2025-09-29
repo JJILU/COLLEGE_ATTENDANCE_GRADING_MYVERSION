7eac7e58a04d9e7e0adf34fe993e9f99157d7577
naomixox <walu9963@gmail.com>
2025-09-29 16:19:56 +0300

    add allowed methods to end points

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,request,render_template, session, url_for
+from flask import Flask, redirect, request, render_template, session, url_for
 from extensions import db, migrate
 
 
@@ -8,49 +8,49 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auth.sqlite3"
 
 # init extensions
 db.init_app(app)
-migrate.init_app(app,db)
+migrate.init_app(app, db)
 
 # make app aware of models
 # from models import User,Profile
 
 
-@app.route("/") 
+@app.route("/")
 def home():
     if 'username' in session:
         return render_template('Dashboard/dashbaord.html')
     return render_template('landing_page.html')
 
 # Lecturer Auth End-points
-@app.route("/lecturer_register", methods=["GET","POST"]) 
+
+
+@app.route("/lecturer_register", methods=["GET", "POST"])
 def lecturer_register():
     if request.method == "GET":
         return render_template('auth/lecturer_auth.html')
 
-    valid_lecturer_id = ["L1001","L1002","L1003","L1004","L1005"]
+    valid_lecturer_id = ["L1001", "L1002", "L1003", "L1004", "L1005"]
 
     # extract auth credentials
 
-    auth_creds =  request.form
+    auth_creds = request.form
 
-    lecture_id = auth_creds.get("lecturerid","").strip()
-    password = auth_creds.get("password","").strip()
+    lecture_id = auth_creds.get("lecturerid", "").strip()
+    password = auth_creds.get("password", "").strip()
 
-    # check if auth credentials was submitted 
-    if not lecture_id and not password :
+    # check if auth credentials was submitted
+    if not lecture_id and not password:
         return render_template('auth/lecturer_auth.html', error="Lecturer ID and password are required!"), 400
-    
 
     # check for individual missing fields
     missing_fields = []
-    
-     # handle missing or empty lecturerid
-    if not auth_creds["lecturerid"] and auth_creds["lecturerid"].strip() == "":
+
+    # handle missing or empty lecturerid
+    if not auth_creds["lecturerid"] or auth_creds["lecturerid"].strip() == "":
         missing_fields.append("lecturer id is a required!")
 
     # handle missing or empty password
-    if not auth_creds["password"] and auth_creds["password"].strip() == "":
-        missing_fields.append("password id is a required!")    
-
+    if not auth_creds["password"] or auth_creds["password"].strip() == "":
+        missing_fields.append("password id is a required!")
 
     if missing_fields:
         return render_template('auth/lecturer_auth.html', missing_fields=missing_fields), 400
@@ -58,32 +58,30 @@ def lecturer_register():
     return "registered as lecturer"
 
 
-@app.route("/lecturer_login") 
+@app.route("/lecturer_login", methods=["GET", "POST"])
 def lecturer_login():
     return "logged as lecturer"
 
 
-
 # Student Auth End-points
-@app.route("/student_register") 
+@app.route("/student_register", methods=["GET", "POST"])
 def student_register():
     return "registered as student"
 
 
-
-@app.route("/student_login") 
+@app.route("/student_login", methods=["GET", "POST"])
 def student_login():
     return "registered as student"
 
 
 # Logout Enpoint
-@app.route("/logout") 
-def  logout():
+@app.route("/logout", methods=["GET"])
+def logout():
     return "logged out"
 
 
 # Dashboard
-@app.route("/dashboard") 
+@app.route("/dashboard")
 def dasboard():
     if "username" in session:
         return render_template('Dashboard/dashbaord.html')
