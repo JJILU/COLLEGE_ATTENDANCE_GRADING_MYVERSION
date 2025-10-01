b24dd00c0df5c5ed55576c3dfb3882be831a4313
naomixox <walu9963@gmail.com>
2025-10-01 11:53:48 +0300

    added registerd student and employed lecturer  models

--- models/attendance_grading.py
+++ models/attendance_grading.py
@@ -3,6 +3,54 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+
+
+
+
+from datetime import datetime
+from faker import Faker
+import random
+
+# --- Association table for Lecturers & Courses ---
+lecturer_course = db.Table(
+    "lecturer_course",
+    db.Column("lecturer_id", db.Integer, db.ForeignKey("employed_lecturers.id")),
+    db.Column("course_id", db.Integer, db.ForeignKey("course.id"))
+)
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    duration = db.Column(db.Integer)
+    present_year = db.Column(db.Integer, default=datetime.now().year)
+
+    students = db.relationship("Student", backref="course")
+
+
+class RegisteredStudents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    student_id = db.Column(db.String(20), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # one-to-many relation with Course
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
+    course = db.relationship("Course", backref="registered_students")
+
+
+class EmployedLecturers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    nrc_number = db.Column(db.String(20), unique=True)
+    lecturer_id = db.Column(db.String(20), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # many-to-many relation with Course
+    courses = db.relationship("Course", secondary=lecturer_course, backref="lecturers")
+
+
 # --- Association Tables ---
 date_class = db.Table(
     "date_class",
@@ -48,20 +96,13 @@ class Student(db.Model):
     semester = db.Column(db.Integer)
 
     # relation with course
-    course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
-
+    
     classes = db.relationship("Class", secondary=student_class, backref="students")
     attendances = db.relationship("StudentAttendance", backref="student")
     grades = db.relationship("StudentGrades", backref="student")
 
 
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    duration = db.Column(db.Integer)
-    present_year = db.Column(db.Integer, default=datetime.now().year)
 
-    students = db.relationship("Student", backref="course")
 
 
 class StudentAttendance(db.Model):
